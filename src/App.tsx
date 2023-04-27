ba3a4027c162982153877c25ebd315d7e7875b56
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-04-27 14:48:00 -0300

    feat: criar novo projeto para estudo de reducer no lugar de states

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import { DeveloperForm } from "./pages/DeveloperForm";
+
+function App() {
+  return (
+    <div className="App">
+      <DeveloperForm />
+    </div>
+  );
+}
+
+export default App;
