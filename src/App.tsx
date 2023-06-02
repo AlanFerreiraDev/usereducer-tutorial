74454e8af349ec10c4a6536593544dd4e9ac673e
Alan Bruno Ferreira da Silva <alanjjr83@gmail.com>
2023-06-02 12:01:42 -0300

    test: teste de git

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-import { DeveloperForm } from "./pages/DeveloperForm";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { DeveloperForm } from './pages/DeveloperForm'
 
 function App() {
   return (
     <div className="App">
       <DeveloperForm />
+      <p>Teste</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
